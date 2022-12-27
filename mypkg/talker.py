08d0c161e00ef5293fb5e4adf728b8fbb52c8497
ryo yanagisawa <s21c1132yt@s.chibakoudai.jp>
2022-12-28 08:57:49 +0900

    a

---
+++ mypkg/talker.py
@@ -0,0 +1,24 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
+
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10)
+n = 0
+def cd ():
+    global n
+    msg = Person()
+    msg.name = "ryo0806"
+    msg.age = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
+
+
+
+
+
+
+
